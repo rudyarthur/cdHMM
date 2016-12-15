6145271e1ff99dad81cc30f34d35eeadecd1f46f
rudyarthur <rudy.d.arthur@gmail.com>
2016-12-15 09:20:02 +0000

    Add makefile, test

---
+++ Makefile
@@ -0,0 +1,27 @@
+CXX=g++
+
+CXXFLAGS = -O3  -std=c++11
+CFLAGS = -O3  -std=c++11
+
+all: test
+
+IFLAGS = -I./include
+
+default: CXXFLAGS = -O3  -std=c++11
+default: CFLAGS = -O3  -std=c++11
+default: all
+
+debug: CXXFLAGS = -g -std=c++11
+debug: CFLAGS =  -g -std=c++11
+debug: all
+
+profile: CXXFLAGS = -pg -O3 -std=c++11
+profile: CFLAGS =  -pg -O3 -std=c++11
+profile: all
+
+test: test.cpp 
+	$(CXX) $(CXXFLAGS) -o test test.cpp $(IFLAGS)
+clean:
+	rm test 
+
+
---
+++ test.cpp
@@ -0,0 +1,622 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <string>
+#include <limits>
+#include <math.h>
+#include "gaussianhmm.hpp" 
+#include "exponentialhmm.hpp" 
+/*#include "poissonhmm.hpp" 
+#include "gammahmm.hpp" 
+#include "lognormalhmm.hpp" 
+#include "discretehmm.hpp" 
+#include "laplacehmm.hpp" 
+#include "jointhmm.hpp" 
+#include "gumbelhmm.hpp" 
+#include "weibullhmm.hpp" 
+#include "extremevaluehmm.hpp" 
+#include "paretohmm.hpp" 
+#include "multihmm.hpp" */
+
+using namespace std;
+
+int main(int argc,char **argv){
+	
+	bool test_gaussian = true;
+	bool test_exponential = false;
+	bool test_gamma = false;
+	bool test_poisson = false;
+	bool test_multinomial = false;
+	bool test_lognormal = false;
+	bool test_joint = false;
+	bool test_laplace = false;
+	bool test_gumbel = false;
+	bool test_weibull = false;
+	bool test_extremevalue = true;
+	bool test_pareto = false;
+	bool test_multi = false;
+	
+	//Gaussian emission
+	if( test_gaussian ) {
+		
+		int N=2;
+		int M=2;
+		
+		vector<vector<double> > A(N, vector<double>(N));
+		vector<vector<double> > B(N, vector<double>(M));
+		vector<double> pi(N);
+		
+		A[0][0] = 0.9; A[0][1] = 0.1; 
+		A[1][0] = 0.1; A[1][1] = 0.9;
+
+		B[0][0] = 0.1; B[0][1] = 0.9;  
+		B[1][0] = 0.9; B[1][1] = 0.1; 
+		
+		pi[0] = 0.6; pi[1] = 0.4;
+	
+		cout << "Gaussian Emission" << endl;
+		gaussianHMM<double> hmm(N,0,1000000); 
+		hmm.init();
+		hmm.A = A; hmm.B = B; hmm.pi = pi;
+		
+		int T = 10000;
+		vector<double> O(T);
+		hmm.generate_seq(O, T, false);
+		
+		hmm.init();
+		hmm.fit(O, 1e-5);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+	}
+	//Exponential emission
+	if( test_exponential ) {
+		
+		int N=2;
+		int M=1;
+		
+		vector<vector<double> > A(N, vector<double>(N));
+		vector<vector<double> > B(N, vector<double>(M));
+		vector<double> pi(N);
+		
+		A[0][0] = 0.9; A[0][1] = 0.1; 
+		A[1][0] = 0.1; A[1][1] = 0.9;
+
+		B[0][0] = 0.1;
+		B[1][0] = 0.9;  
+		
+		pi[0] = 0.6; pi[1] = 0.4;
+		
+		cout << "Exponential Emission" << endl;
+
+		exponentialHMM<double> hmm(N,0,1000000); 
+		hmm.lb[0] = 5;
+		hmm.lb[1] = 5;
+		hmm.init();
+
+		hmm.A = A; hmm.B = B; hmm.pi = pi;
+		
+		int T = 10000;
+		vector<double> O(T);
+		hmm.generate_seq(O, T, false);
+		
+		hmm.init();
+		hmm.fit(O, 1e-10);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+	}
+	//Gamma emission
+	/*if( test_gamma ) {
+		
+		int N=2;
+		int M=2;
+		
+		vector<vector<double> > A(N, vector<double>(N));
+		vector<vector<double> > B(N, vector<double>(M));
+		vector<double> pi(N);
+		
+		A[0][0] = 0.9; A[0][1] = 0.1; 
+		A[1][0] = 0.1; A[1][1] = 0.9;
+		
+		B[0][0] = 0.5; B[0][1] = 1.0;  
+		B[1][0] = 4.5; B[1][1] = 1.5; 
+		
+		pi[0] = 0.6; pi[1] = 0.4;
+		
+		cout << "Gamma Emission" << endl;
+
+		gammaHMM<double> hmm(N,0,1000000); 
+		hmm.init();
+
+		hmm.A = A; hmm.B = B; hmm.pi = pi;
+		
+		int T = 10000;
+		vector<double> O(T);
+		hmm.generate_seq(O, T, false);
+		
+		hmm.init();
+		hmm.fit(O, true, 1e-5);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+	}//Poisson emission
+	if( test_poisson ) {
+		
+		int N=2;
+		int M=1;
+		
+		vector<vector<double> > A(N, vector<double>(N));
+		vector<vector<double> > B(N, vector<double>(M));
+		vector<double> pi(N);
+		
+		A[0][0] = 0.9; A[0][1] = 0.1; 
+		A[1][0] = 0.1; A[1][1] = 0.9;
+
+		B[0][0] = 1;
+		B[1][0] = 10;  
+		
+		pi[0] = 0.6; pi[1] = 0.4;
+		
+		cout << "Poisson Emission" << endl;
+
+		poissonHMM<int> hmm(N,0,1000000); 
+		hmm.init();
+
+		hmm.A = A; hmm.B = B; hmm.pi = pi;
+		
+		int T = 10000;
+		vector<int> O(T);
+		hmm.generate_seq(O, T, false);
+		
+		hmm.init();
+		hmm.fit(O, false, 1e-10);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+		
+		hmm.init();
+		hmm.fit(O, true, 1e-10);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+	} 
+	//Categorical
+	if( test_multinomial ) {
+		
+		int N=2;
+		int M=3;
+		
+		vector<vector<double> > A(N, vector<double>(N));
+		vector<vector<double> > B(N, vector<double>(M));
+		vector<double> pi(N);
+		
+		A[0][0] = 0.9; A[0][1] = 0.1; 
+		A[1][0] = 0.1; A[1][1] = 0.9;
+
+		B[0][0] = 0.1; B[0][1] = 0.4; B[0][2] = 0.5;
+		B[1][0] = 0.8; B[1][1] = 0.0; B[1][2] = 0.2;
+		
+		pi[0] = 0.6; pi[1] = 0.4;
+		
+		cout << "Multinomial Emission" << endl;
+
+		multinomialHMM<int> hmm(N,M,0,1000000); 
+		hmm.init();
+
+		hmm.A = A; hmm.B = B; hmm.pi = pi;
+		
+		int T = 10000;
+		vector<int> O(T);
+		hmm.generate_seq(O, T, false);
+		
+		hmm.init();
+		hmm.fit(O, false, 1e-10);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+		
+		hmm.init();
+		hmm.fit(O, true, 1e-10);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+	}
+	//Log Normal
+	if( test_lognormal ) {
+		
+		int N=2;
+		int M=2;
+		
+		vector<vector<double> > A(N, vector<double>(N));
+		vector<vector<double> > B(N, vector<double>(M));
+		vector<double> pi(N);
+		
+		A[0][0] = 0.9; A[0][1] = 0.1; 
+		A[1][0] = 0.1; A[1][1] = 0.9;
+
+		B[0][0] = 0; B[0][1] = 0.1;
+		B[1][0] = 10; B[1][1] = 0.1; 
+		
+		pi[0] = 0.6; pi[1] = 0.4;
+		
+		cout << "Lognormal Emission" << endl;
+
+		lognormalHMM<double> hmm(N,0,1000000); 
+		hmm.init();
+
+		hmm.A = A; hmm.B = B; hmm.pi = pi;
+		
+		int T = 10000;
+		vector<double> O(T);
+		hmm.generate_seq(O, T, false);
+		
+		hmm.init();
+		hmm.fit(O, false, 1e-10);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+		
+		hmm.init();
+		hmm.fit(O, true, 1e-10);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+	}
+	//joint emission
+	if( test_joint ) {
+		
+		int N=2;
+		int M=2;
+		
+		vector<vector<double> > A(N, vector<double>(N));
+		vector<double> pi(N);
+		
+		A[0][0] = 0.9; A[0][1] = 0.1; 
+		A[1][0] = 0.1; A[1][1] = 0.9;
+
+		vector<vector<double> > Bexp(1, vector<double>(1));
+		Bexp[0][0] = 100;  
+		vector<vector<double> > Bnorm(1, vector<double>(2));
+		Bnorm[0][0] = 3; Bnorm[0][1] = 0.1; 
+		
+		pi[0] = 0.6; pi[1] = 0.4;
+	
+		exponentialHMM<double> hmm_exp(1,0,1000000); 
+		gaussianHMM<double> hmm_norm(1,0,1000000); 
+		jointHMM<double> hmm(&hmm_exp,&hmm_norm,0,1000000); 
+		hmm.init();
+		hmm.A = A; hmm.pi = pi;
+		hmm_exp.B = Bexp; hmm_norm.B = Bnorm;
+
+		cout << "Joint Emission" << endl;
+
+		int T = 10000;
+		vector<double> O(T);
+		hmm.generate_seq(O, T, false);
+		
+		
+		hmm.init();
+		hmm.fit(O, false);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+		
+		hmm.init();
+		hmm.fit(O, true);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+	}
+	//Laplace
+	if( test_laplace ) {
+		
+		int N=2;
+		int M=2;
+		
+		vector<vector<double> > A(N, vector<double>(N));
+		vector<vector<double> > B(N, vector<double>(M));
+		vector<double> pi(N);
+		
+		A[0][0] = 0.9; A[0][1] = 0.1; 
+		A[1][0] = 0.1; A[1][1] = 0.9;
+
+		B[0][0] = 0; B[0][1] = 1;
+		B[1][0] = 15; B[1][1] = 0.5; 
+		
+		pi[0] = 0.6; pi[1] = 0.4;
+		
+		cout << "Laplace Emission" << endl;
+
+		laplaceHMM<double> hmm(N,0,1000000); 
+		hmm.init();
+
+		hmm.A = A; hmm.B = B; hmm.pi = pi;
+		
+		srand(1234567);
+		
+		int T = 10000;
+		vector<double> O(T);
+		hmm.generate_seq(O, T, false);
+		
+		hmm.init(); 
+		hmm.fit(O, false, 1e-10);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+		
+		hmm.init(); 
+		hmm.fit(O, true, 1e-10);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+		
+	}
+	//Gumbel
+	if( test_gumbel ) {
+		
+		int N=2;
+		int M=2;
+		
+		vector<vector<double> > A(N, vector<double>(N));
+		vector<vector<double> > B(N, vector<double>(M));
+		vector<double> pi(N);
+		
+		A[0][0] = 0.9; A[0][1] = 0.1; 
+		A[1][0] = 0.1; A[1][1] = 0.9;
+
+		B[0][0] = 0.5; B[0][1] = 2;
+		B[1][0] = 40; B[1][1] = 4; 
+		
+		pi[0] = 0.6; pi[1] = 0.4;
+		
+		cout << "Gumbel Emission" << endl;
+
+		gumbelHMM<double> hmm(N,0,1000000); 
+		hmm.init();
+
+		hmm.A = A; hmm.B = B; hmm.pi = pi;
+		
+		srand(123456789);
+		
+		int T = 10000;
+		vector<double> O(T);
+		hmm.generate_seq(O, T, false);
+		
+		hmm.init(); 
+		hmm.fit(O, false, 1e-5);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+
+		hmm.init(); 
+		hmm.fit(O, true, 1e-5);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+			
+	}
+	//Weibull
+	if( test_weibull ) {
+		
+		int N=2;
+		int M=2;
+		
+		vector<vector<double> > A(N, vector<double>(N));
+		vector<vector<double> > B(N, vector<double>(M));
+		vector<double> pi(N);
+
+		A[0][0] = 0.9; A[0][1] = 0.1; 
+		A[1][0] = 0.1; A[1][1] = 0.9;
+
+		B[0][0] = 5; B[0][1] = 1;
+		B[1][0] = 0.9; B[1][1] = 2; 
+
+		pi[0] = 0.6; pi[1] = 0.4;
+		
+		cout << "Weibull Emission" << endl;
+
+		weibullHMM<double> hmm(N,0,1000000); 
+		hmm.init();
+
+		hmm.A = A; hmm.B = B; hmm.pi = pi;
+				
+		srand(123456789);
+		
+		int T = 10000;
+		vector<double> O(T);
+		hmm.generate_seq(O, T, false);
+		
+		hmm.init(); 
+		
+		hmm.fit(O, false, 1e-5);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+
+		hmm.init(); 
+		hmm.fit(O, true, 1e-5);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+			
+	}
+	//Extreme value
+	if( test_extremevalue ) {
+		
+		int N=2;
+		int M=3;
+		
+		vector<vector<double> > A(N, vector<double>(N));
+		vector<vector<double> > B(N, vector<double>(M));
+		vector<double> pi(N);
+
+		//A[0][0] = 1;
+		A[0][0] = 0.9; A[0][1] = 0.1; 
+		A[1][0] = 0.1; A[1][1] = 0.9;
+
+		B[0][0] = 10000; B[0][1] = 3000; B[0][2] = 0.125;
+		B[1][0] = -10000; B[1][1] = 3000; B[1][2] = 0.125 ;
+
+		//pi[0] = 1;
+		pi[0] = 0.6; pi[1] = 0.4;
+		
+		//test GEV functions
+		/*  gsl_vector *theta;
+		  theta = gsl_vector_alloc (3);
+		  gsl_vector_set (theta, 0, B[0][0]);
+		  gsl_vector_set (theta, 1, B[0][1]);
+		  gsl_vector_set (theta, 2, B[0][2]);
+		  
+		  gsl_vector *dl = gsl_vector_alloc (3);
+		  gsl_matrix *J = gsl_matrix_alloc (3,3);
+   
+		  vector<double> O = {9000, 9500, 10000};
+          double nlog = GEV_Lhood (theta, O, dl, J);
+     
+          cout << nlog << "\n" << endl;
+          cout << gsl_vector_get (dl, 0) << " " << gsl_vector_get (dl, 1) << " " << gsl_vector_get (dl, 2) << "\n" << endl;
+          cout << gsl_matrix_get (J, 0, 0) << " " << gsl_matrix_get (J, 0, 1) << " " << gsl_matrix_get (J, 0, 2) << endl;
+          cout << gsl_matrix_get (J, 1, 0) << " " << gsl_matrix_get (J, 1, 1) << " " << gsl_matrix_get (J, 1, 2) << endl;
+          cout << gsl_matrix_get (J, 2, 0) << " " << gsl_matrix_get (J, 2, 1) << " " << gsl_matrix_get (J, 2, 2) << endl;
+        */
+        
+      
+		//cout << "Extreme Value Emission" << endl;
+/*
+		extremevalueHMM<double> hmm(N,0,1000000); 
+		hmm.init();
+
+		hmm.A = A; hmm.B = B; hmm.pi = pi;
+			
+					
+		srand(123456789);
+		
+		int T = 10000;
+		vector<double> O(T);
+		hmm.generate_seq(O, T, false);
+          
+		hmm.init(); 
+		hmm.print_iter = true;
+		hmm.solver_eps = 1e-6;
+		hmm.solver_max_iter = 1000;
+		hmm.B = B;
+		hmm.fit(O, true, 1e-5);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+			
+	}//Pareto
+	if( test_pareto ) {
+		
+		int N=2;
+		int M=1;
+		
+		vector<vector<double> > A(N, vector<double>(N));
+		vector<vector<double> > B(N, vector<double>(M));
+		vector<double> pi(N);
+		
+		//A[0][0] = 1;
+		A[0][0] = 0.9; A[0][1] = 0.1; 
+		A[1][0] = 0.1; A[1][1] = 0.9;
+
+		B[0][0] = 2; 
+		B[1][0] = 1; 
+		
+		//pi[0] = 1;
+		pi[0] = 0.6; pi[1] = 0.4;
+		
+		//cout << "Pareto Emission" << endl;
+
+		paretoHMM<double> hmm(N,0,1000000); 
+		hmm.init();
+
+		hmm.A = A; hmm.B = B; hmm.pi = pi;
+		hmm.lb[0] = 1;
+		hmm.lb[1] = 1;
+			
+		int T = 10000;
+		vector<double> O(T);
+		hmm.generate_seq(O, T, false);
+		
+		hmm.init(); 
+		hmm.fit(O, false, 1e-5);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+		
+		hmm.init();
+		hmm.fit(O, true, 1e-5);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+	}//multi emission
+	if( test_multi ) {
+		
+		int N=3;
+		int M=2;
+		
+		vector<vector<double> > A(N, vector<double>(N));
+		vector<double> pi(N);
+		
+		//A[0][0] = 1;
+
+		//A[0][0] = 0.9; A[0][1] = 0.1; 
+		//A[1][0] = 0.1; A[1][1] = 0.9;
+		
+		A[0][0] = 0.9;  A[0][1] = 0.05;  A[0][2] = 0.05; 
+		A[1][0] = 0.05; A[1][1] = 0.9;   A[1][2] = 0.05; 
+		A[2][0] = 0.05; A[2][1] = 0.05;  A[2][2] = 0.9; 
+
+		vector<vector<double> > Bexp(1, vector<double>(1));
+		Bexp[0][0] = 100;  
+		vector<vector<double> > Bnorm(1, vector<double>(2));
+		Bnorm[0][0] = 3; Bnorm[0][1] = 0.1; 
+		vector<vector<double> > Blognorm(1, vector<double>(2));
+		Blognorm[0][0] = 5; Blognorm[0][1] = 1; 
+			
+		//pi[0]=1;
+		//pi[0] = 0.6; pi[1] = 0.4;
+		pi[0] = 0.6; pi[1] = 0.2; pi[2] = 0.2;
+	
+		exponentialHMM<double> hmm_exp(1,0,1000000); 
+		gaussianHMM<double> hmm_norm(1,0,1000000); 
+		lognormalHMM<double> hmm_lognorm(1,0,1000000);
+		//vector< HMM<double>* > hmms = { &hmm_exp };
+		//vector< HMM<double>* > hmms = { &hmm_exp, &hmm_norm };
+		vector< HMM<double>* > hmms = { &hmm_exp, &hmm_norm, &hmm_lognorm };
+		
+		multiHMM<double> hmm(hmms,0,1000000); 
+		hmm.init();
+		hmm.A = A; hmm.pi = pi;
+		hmm_exp.B = Bexp; hmm_norm.B = Bnorm; hmm_lognorm.B = Blognorm;
+
+		//cout << "Joint Emission" << endl;
+
+		int T = 30000;
+		vector<double> O(T);
+		hmm.generate_seq(O, T, false);
+		
+		
+		hmm.init();
+		hmm.fit(O, false, 1e-10);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+		
+		hmm.init();
+		hmm.fit(O, true, 1e-10);
+		cout << "A = "; hmm.printA();
+		cout << "B = "; hmm.printB();
+		cout << "pi = "; hmm.printpi();
+	}
+	
+	return 0;
+	*/
+	
+} 
+
+
